176820094939f33f1290b8ca9997625335eee8eb
Christopher <kung_cool_yes@hotmail.com>
2015-12-22 11:41:42 +0100

    Gjort små ändringar

--- Labbar/Labb 1/Scritp/PundTillKronor.js
+++ Labbar/Labb 1/Scritp/PundTillKronor.js
@@ -1,1 +1,16 @@
-﻿
+﻿var pund = document.getElementById('Pund');
+var dollar = document.getElementById('dollar');
+
+var berakningPund = calculatePundSvar(pund);
+document.getElementById('PundSvar').innerHTML = berakning;
+
+var berakningKR = calculatePundSvar(pund, kronor);
+document.getElementById('dollarSvar').innerHTML = berakning;
+
+function calculatePundSvar(pund) {
+    return pund * 6 ;
+}
+
+function calculatePundSvar(pund, kronor) {
+    return dollar * 12;
+}
