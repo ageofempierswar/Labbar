c1fbc373e21ffb4b2083c80fed688c02fe0435d9
Christopher <kung_cool_yes@hotmail.com>
2016-01-03 20:19:55 +0100

    Fixat del 4

--- Labbar/Labb 1/Scritp/Del 4.js
+++ Labbar/Labb 1/Scritp/Del 4.js
@@ -1,1 +1,13 @@
-﻿
+﻿var attBetala = document.getElementById("att_betala").addEventListener("click", skrivUtSumma, false);
+
+function skrivUtSumman() {
+    var pris = document.getElementById("pris").value;
+    document.getElementById("att_betala").innerHTML = räknaUtSumman(pris);
+}
+
+function räknaUtSumman(pris) {
+    if (pris >= 1000)
+        return pris * 0.90;
+    else
+        return pris;
+}
