aff1ef632cbd9ddc9de1a423574bdc0d98a52355
Christopher <kung_cool_yes@hotmail.com>
2016-01-03 22:37:17 +0100

    del 5 klar =D

--- Labbar/Labb 1/Scritp/Del 5.js
+++ Labbar/Labb 1/Scritp/Del 5.js
@@ -1,1 +1,6 @@
-﻿
+﻿var summa = document.getElementById('summa2').addEventListener('click', skrivUtSumman, false);
+
+function skrivUtSumman() {
+    var tal = document.getElementById('tal').value;
+    document.getElementById('summa2').innerHTML = ((Number(tal) * 3) + 7);
+}
