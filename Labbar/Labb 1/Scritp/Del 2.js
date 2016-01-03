a7737f75045a999b1dd2fd689079a5dec96ef0c4
Christopher <kung_cool_yes@hotmail.com>
2016-01-03 13:17:22 +0100

    La till Javaskripts koden till del 2 =D

--- Labbar/Labb 1/Scritp/Del 2.js
+++ Labbar/Labb 1/Scritp/Del 2.js
@@ -1,1 +1,11 @@
-﻿
+﻿var lön = document.getElementById("lön").addEventListener("click", bruttoLön, false);
+var brutto = document.getElementById('brutto').innerHTML;
+
+function bruttoLön() {
+    var försäljning = document.getElementById("försäljningssumman").value;
+    document.getElementById("lön").innerHTML = Calculate(brutto, försäljning);
+}
+
+function Calculate(brutto, försäljning) {
+    return Number(brutto) + (Number(försäljning) * 0.09);
+}
