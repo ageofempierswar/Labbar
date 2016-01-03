207b3c376964c3e1a13a9a180e092e9fb0426a72
Christopher <kung_cool_yes@hotmail.com>
2016-01-03 16:03:01 +0100

    Fixa lite i HTML och lagt till allt i del 3 =D

--- Labbar/Labb 1/Scritp/Del 3.js
+++ Labbar/Labb 1/Scritp/Del 3.js
@@ -1,1 +1,10 @@
-﻿
+﻿var calculateTimeButton = document.getElementById('calculateTidButton');
+
+function CalculateTime() {
+    var Hours = document.getElementById('timmar').value;
+    var minutes = Number(Hours) * Number(60);
+    var seconds = Number(Hours) * Number(3600);
+    document.getElementById('minuter').innerHTML = minutes;
+    document.getElementById('secunder').innerHTML = seconds;
+}
+calculateTimeButton.addEventListener('click', CalculateTime, false);
