6560617997227beac9841790f168f9eae1061b76
Christopher <kung_cool_yes@hotmail.com>
2016-01-08 14:59:47 +0100

    Klar med  Scripten

--- Labbar/Labb 3/Scripts/Index.js
+++ Labbar/Labb 3/Scripts/Index.js
@@ -3,3 +3,27 @@
     $("#search_input").keyup(function () { loadJson(); });
     $(".checkboxes").change(function () { loadJson(); });
 });
+
+function loadJson() {
+    var output = "<ul class='resultList'>";
+    var searchInput = $("#search_input").val();
+    var regex = new RegExp(searchInput, "i");
+    $.getJSON("Json/products.json", function (data) {
+        $.each(data, function (key, value) {
+            if ((value.name.search(regex) !== -1) || (value.description.search(regex) !== -1)) {
+                if ($("#" + value.category).is(":checked")) {
+                    output += "<li>";
+                    output += "<img src='" + value.img + "' alt='" + value.name + "'/>";
+                    output += "<h2>" + value.name + "</h2>" + "</br>";
+                    output += value.description;
+                    output += "<br/><br/>";
+                    output += "Price: " + value.price;
+                    output += "<br/> Size: " + value.size;
+                    output += "</li>";
+                }
+            }
+        });
+        output += "</ul>";
+        $("#searchResults").html(output);
+    });
+}
